ac74032f631e504c098f2b1468f3ae5adbd01077
For3vigt <nickwelles@live.nl>
2024-11-28 11:34:41 +0100

    fix: :bug: Fix API URL env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
   console.log("Runtime API URL:", window.REACT_APP_API_URL);
-  return window.REACT_APP_API_URL || "http://localhost:8002/api";
+  return window.REACT_APP_API_URL || "http://localhost:8003/api";
 };
 
 export const config = {
